0d2252f37fe46fb20e19ecf763c0421445059ef3
kbazant <krizz100@yahoo.com>
2017-06-01 00:28:45 +0100

    Added first recipe item.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe/model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/01/14/17/46/eat-1140371_1280.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
